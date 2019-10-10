958f3352f7203fc7919980bcb273f396ad4a16a1
Piotr Solnica <piotr.solnica@gmail.com>
2019-10-10 10:15:22 +0200

    Make workflow CLI command actually do something

--- lib/ossy/cli/github/workflow.rb
+++ lib/ossy/cli/github/workflow.rb
@@ -1,18 +1,28 @@
 # frozen_string_literal: true
 
 require 'ossy/cli/commands/core'
+require 'ossy/import'
 
 module Ossy
   module CLI
     module Github
       class Workflow < Commands::Core
+        include Import['github.workflow']
+
         desc 'Start a GitHub workflow'
 
         argument :repo, required: true, desc: 'The name of the repository on GitHub'
         argument :name, required: true, desc: 'The name of the workflow'
 
         def call(repo:, name:)
-          puts "Workflow: #{repo} => #{name}"
+          puts "Requesting: #{repo} => #{name}"
+          result = workflow.(repo, name)
+
+          if result.code.eql?(204)
+            puts 'Success!'
+          else
+            puts "Failure! #{result.inspect}"
+          end
         end
       end
     end

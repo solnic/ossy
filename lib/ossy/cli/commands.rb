65bc693b93e1707ae81897a8aa2a9c9137016d01
Piotr Solnica <piotr.solnica@gmail.com>
2019-10-11 10:23:27 +0200

    Add "github update_file" to CLI

--- lib/ossy/cli/commands.rb
+++ lib/ossy/cli/commands.rb
@@ -8,9 +8,11 @@ module Ossy
       extend Hanami::CLI::Registry
 
       require 'ossy/cli/github/workflow'
+      require 'ossy/cli/github/update_file'
 
       register 'github', aliases: %w[gh] do |github|
         github.register 'workflow', Github::Workflow, aliases: %w[w]
+        github.register 'update_file', Github::UpdateFile, aliases: %w[uf]
       end
     end
   end
